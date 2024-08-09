32caa3d18398711970d3a6fe335af9e75e9bba37
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-10 01:45:47 +0600

    create field components

--- src/components/FieldSet.jsx
+++ src/components/FieldSet.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const FieldSet = ({ label, children }) => {
   return (
-    <fieldset>
-      {label && <legend>{label}</legend>}
-      <div>{children}</div>
+    <fieldset className="m-2 border-none p-0">
+      {label && <legend className="text-md font-bold mb-2">{label}</legend>}
+      <div className="flex flex-col justify-between self-start">{children}</div>
     </fieldset>
   );
 };

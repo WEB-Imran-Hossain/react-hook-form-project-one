82139da4027e68e00b40817c3d9282452e594929
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-12 00:33:55 +0600

    create social field

--- src/forms/RegistrationForm.jsx
+++ src/forms/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FieldSet from "../components/FieldSet";
 import Field from "../components/Field";
-import { useForm } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 
 const RegistrationForm = () => {
     const {
@@ -9,16 +9,24 @@ const RegistrationForm = () => {
         handleSubmit,
         formState: { errors },
         setError,
+        control
     } = useForm()
 
     const submitForm = (formData) => {
         console.log(formData);
     }
 
+    // create dynamic field
+    const { fields, append, remove } = useFieldArray({
+        name: "socials",
+        control
+
+    })
+
     return (
         <div className="flex flex-col justify-center items-center w-screen h-screen">
             <form onSubmit={handleSubmit(submitForm)}>
-                <FieldSet label="Enter Your Details">
+                <FieldSet label="Enter Basic Details">
                     {/* name section */}
                     <Field label="Full Name" error={errors.fname}>
                         <input
@@ -33,6 +41,7 @@ const RegistrationForm = () => {
 
                         />
                     </Field>
+
                     {/* email section */}
                     <Field label="Email" error={errors.email}>
                         <input
@@ -45,6 +54,7 @@ const RegistrationForm = () => {
 
                         />
                     </Field>
+
                     {/* pasword section */}
                     <Field label="Password" error={errors.password}>
                         <input
@@ -82,6 +92,50 @@ const RegistrationForm = () => {
 
                         />
                     </Field>
+
+                </FieldSet>
+                {/* social handle */}
+                <FieldSet label="Enter Social Handles">
+                    {fields.map((field, index) => {
+                        return (
+                            <div
+                                className="flex justify-between items-center w-max"
+                                key={field.id}
+                            >
+                                <Field label="Social Name">
+                                    <input
+                                        className="p-2 border box-border w-full rounded-md"
+                                        type="text"
+                                        {...register(`socials[${index}].name`)}
+                                        id={`socials[${index}].name`}
+                                        name={`socials[${index}].name`}
+                                    />
+                                </Field>
+
+                                <Field label="Social URL">
+                                    <input
+                                        className="p-2 border box-border w-full rounded-md"
+                                        type="text"
+                                        {...register(`socials[${index}].url`)}
+                                        id={`socials[${index}].url`}
+                                        name={`socials[${index}].url`}
+                                    />
+                                </Field>
+                                <button
+                                    className="mt-8 mr-2 text-2xl"
+                                    onClick={() => remove(index)}
+                                >
+                                    &#8722;
+                                </button>
+                            </div>
+                        );
+                    })}
+                    <button
+                        className="mt-8 text-md text-white cursor-pointer border rounded-lg bg-gray-500 p-1 m-auto"
+                        onClick={() => append({ name: "", url: "" })}
+                    >
+                        Add A Social Handle
+                    </button>
                 </FieldSet>
                 <div>{errors?.root?.random?.message}</div>
                 <Field>
